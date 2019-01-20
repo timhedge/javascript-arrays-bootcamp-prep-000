b9ac64738c841f45f5d6cacb5e662c239df75402
Timothy Hedge <timwhedge@gmail.com>
2019-01-20 20:21:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
 function addElementToBeginningOfArray(array,element) 
 {
-  var newarray = array.unshift(element)
+  var newarray = [element, ...array]
   return newarray
 }
 
