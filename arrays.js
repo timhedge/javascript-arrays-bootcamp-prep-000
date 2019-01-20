1eb1166feb8a91627391e1480c591094fb6549c3
Timothy Hedge <timwhedge@gmail.com>
2019-01-20 19:50:38 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,element) 
+{
+  var newarray = array.unshift(element)
+  return newarray
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element)
+{
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array,element)
+{
+  var newarray = array.push(element)
+  return newarray
+}
+
+function destructivelyAddElementToEndOfArray(array,element)
+{
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index)
+{
+  return array[parseInt(index)]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array)
+{
+  var removeFirstElement = array.slice(1)
+  return array
+}
